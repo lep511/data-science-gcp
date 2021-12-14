a6a7c91a276cf28b1a06869ce2d788d7b6b2d63d
Esteban Perez <46011885+lep511@users.noreply.github.com>
2021-12-14 20:31:48 -0300

    Add files via upload

---
+++ sql_made.sh
@@ -0,0 +1,14 @@
+PROJECT=$(gcloud config get-value project)
+BUCKET=$PROJECT_ID-ml
+
+gcloud sql instances create flights \
+--database-version=POSTGRES_13 --cpu=2 --memory=8GiB \
+--zone=us-central1-f --root-password=n23w5k3GoAvev4mw
+
+gcloud sql databases create bts \
+--instance=flights
+
+gcloud sql databases list \
+--instance=flights
+
+gsutil cp create_table.sql gs://$BUCKET/flights/sql/create_table.sql
